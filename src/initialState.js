1be80d3cd8292764302e5f9d7d1a766ff4a91aec
Jonathan Garcia <jona03g97@gmail.com>
2021-02-01 09:51:53 -0600

    Cambiando dirección de imagenes principales

--- src/initialState.js
+++ src/initialState.js
@@ -5,7 +5,7 @@ export default {
     {
       id: 1,
       images: [
-        'https://lh3.googleusercontent.com/zy-P0cDsav-QPzz5v9Ha4kPllU6COBTtdSoUjlflopuWfan9Iu3HOPTQBf2wHGKbe1CjJTpKTuZRF_WWBiGo4_bvRHampEMAMlzQmN30mhr8pwnE_GWVS_pCe6nQihMv7kl0rXutR7xV4pxLKV6ebhS0UKUFllr462ApQMB7GKsF5FYaXmZuBYVEJHlApmlshWYqY-OZLaRAJ1JFLgexIT6yXF2nX6E1AOgCpat3imBcjplBz6XL3wanEc86vQB9NIvU3khGs54Y41PWSX6FboRYyPmMNG0AkgsdkN7cgq_9ks4hwEdKKBpW8G9tr-8tZLGIZMNyzITD52fyvDzsF5ovcr1ixsxd5ucsMG6zRC07gEKn7dSaJxG9mEtkqs5QrFoNkPiKVQI7wjdXLx5JfWFEJ5G4E62vEertAx_EOxRNAOXw5dLeqkqXkKwT41hGJTP_O4gdxr939o8zjBMb3NlUvjVhjiRS2ITANZs-44iTXPoMwCTOtNRjtHl-VkgQZ9hkHd_u19rfpGt3nHxkJTti8qAl_TRGABCW8YxJxQ-Wk_BlVhO_pCPApGPEm4PPdd9YBbI2lAOX4zyovxB1hw4Do6csu9NM0ulicyNJ8DLto4aTa_dnN9pa-q_vSL3Wd0Qscqpew4aRLFAUAp1fo2bKhJLoyEQm15hw1wG5axq_bRuXdDaoPOB_h-Dnpw=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/145705751_3747021038711798_5290724554064654964_o.jpg?_nc_cat=109&ccb=2&_nc_sid=0debeb&_nc_ohc=NAdl74TViZ4AX8iiNrA&_nc_ht=scontent.fsyq2-1.fna&oh=6d749c2f8a38f67515dc6663c2749b1f&oe=603C8EB1',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -18,7 +18,7 @@ export default {
     {
       id: 2,
       images: [
-        'https://lh3.googleusercontent.com/1jbctpsKFWBb4kYJTt0kVKEWCEvEERwlokIq12fdwMG0-xoTdWngvNcLc2AhtBYXZojF4ofvkbAiYUdmm90YOCSaQIpblBBjlPP18GqKeQwfqKQtfetLa3Au92XWo3apPl-z5dBgvXpIL7tJVDtx_BNNiQr5T9xRBj0EOk6OeriUz0yaEuXCIdJrBftHRJuv3_1_dkvpA0PvD5EnKdntOcptLDisYpq1YSzIvV734N0D9MVjgvBNLRayDJK9ORyVlXe4-5h7h3qp4VeTNM4VINaNXSzVGJQimxtkrAZTX436Wib1V1Zzgl6LtpqGOK141UbdhBHcAZxOp5E04q8p7KbDfMdnLNJF3LTwgvNbTw8ihtl9QluIosILvgODAKDuVLo1alc1cmaOi7f9MXUizJl6D2ijf2KvPECHkb-moU3pIdLjImhtIGpMqf9pO5XGF3EqG92JeTnYkHD4yD2X-t5GT3ocJqZGjn_OfqCHy-dJd_rg6y770TcCA-1bPHjeEPVAqGYHO3asWlVpPWCk9jUoQVAJ8MiPIZgkQ8-QNs7YbEPGObMe5bxw0w_YNDIAHbyxF_xX59P4ga19MI9akEXQ8ps-q8i0YpPc5p1v6OAT4lQ-twjZi_Jp8wDmHFAukmDeIbaaXjXOIG_u3EyX1XkfB4KpW88k_QW1jVAH-UVzvt3XNFtI1iK-xx_RxA=w157-h209-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/144767677_3747021935378375_2435789306155617636_o.jpg?_nc_cat=109&ccb=2&_nc_sid=0debeb&_nc_ohc=QXNJ5aUpLuYAX-9LGeA&_nc_ht=scontent.fsyq2-1.fna&oh=f0a7ca38be366b90e1c1323a2425ade8&oe=603DE4E6',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -31,7 +31,7 @@ export default {
     {
       id: 3,
       images: [
-        'https://lh3.googleusercontent.com/5gumh3JdFBd1Xy1YBA67iUEXnLy_P9Dqgdlt20XXL8lKMnbSEX4j7vS2fdfBJoaPtGXAweXJo4P19Zs1sOxXSCDX6DF7qsEdJ_dvvYjUOn1n6M9YuLYfCQX3x9pSkqlF5CnUOssVeSqhvPvSNP8F3jWbDeNOk_u2cOt9F-0fe1Agjmr9Aj5WZRuzL9VjyZ6zqCLHjCJh3L0smeLtaXeCt5S4Vf1GsBRWfw_nQ7ei-u3b0NjdH8l6Ub4dy6bYWkKd564ZZCm-xroL8fZbpZFcqSPB8ptwyMYnAF3oade3ekl3hJiJuXv5ague8GvTQz3xZfDgKMxj_GGFKY8LyC6G3J-fTruQKlaDQoZ9fBkMCJIyG--NLrGW8Y-Bi2MK6iOX4QlO2Mag3k29962Dm3ijTO6V41RuwTCqpf7FQOEvEMnL5Fdobt_NmFxKSvZWt5SklCIZabyOx7SwDZo_dpwGFmVYuEDlWqq-LpLi5ujevJ32X2NGlrKh_x2033lZc3dh993BTbFxiQSxLlDTZzi0js0f4vE4DPsFA7Ub6ozY7ayOokG1BkCJLctrw05yFdJfp4JywJTM9s0TuXRiSbxKHiSDruBEoF1j_LQBWWp3dgUUPlP01rnc3rPoyn45mYRM73MKyK71wj7zMfMnQ8mV0FJMGkSgcR5JMBM9t_jBWzV6_dDAh0Wg5mND5PQJRA=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/144227050_3747021895378379_974847196028160846_o.jpg?_nc_cat=100&ccb=2&_nc_sid=0debeb&_nc_ohc=6GfwgBKpqyAAX8GHHRJ&_nc_ht=scontent.fsyq2-1.fna&oh=77365c26c82f868a44b77f8c16e9bcc5&oe=603E1DA9',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -43,7 +43,7 @@ export default {
     {
       id: 4,
       images: [
-        'https://lh3.googleusercontent.com/R69nB5bqaq0rfkSWGMHXv2ORZm8FfWKV46kdB8gDplrGUw3QUHxSGhAY1rwWRKIb1h2sRJ6L_P4cRVHvlQshqya8Aev4MAne7noFtqUyuhXwv-70udxfxApbWMNyJQdkT01xOa9M-mMZ8uIxylYqYxM8giBxombtUwfzWZ-FS_BSvWj16lMnRJgFsj58srVUx06S1Az7zME6ewpwtCQDXoKCL2dnHC0416UtLZcM-nLsujELVPJAFNOnZ8mKtwqBjACw7c18cREeSKqGGZQ3bMENh5tKy6ebA18hU5Ag0PBvk18_dDa-ID5QfCN48qhaXg6lDqqaNe1-vMbY8gWIG-j4P0k0KebWn65LdvJZCbiltk2AKMtticwXegPFmCnaj9-ZvJDxGTMW2yifkz-N4sirwZbhQrewgM8peJ_gQUpaRVBKDkcCJXa1Ve7B6U7InvP00N7j1VoobC50MM81GH-lV9O-TgsUNglBx5G0b-UQx06GTeNLv3I4gpJCbFElAqGqwQGJhucOz-QJPsJ56DBVQ6tA5rCRlKmXGGwUq1wGMWyKN2E7LhPuvuckVHEOuVhXQXjlhBRixJa0jr4NF94WDsLQ-X5zRPcUCaAAVZNs-dK9twFdPn1u6ce_SGx4diFK44zWHJRFeVWkuuexyzXvRcHZFmCWoqE1mZdn1upqUcR_ZQwmFaNoGz2NCg=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/145111611_3747022025378366_8917091535458350245_o.jpg?_nc_cat=104&ccb=2&_nc_sid=0debeb&_nc_ohc=Zn1tfCdOTo4AX_Vu4zu&_nc_ht=scontent.fsyq2-1.fna&oh=8c26822b05d4e8e68209ce67e92f3bb8&oe=603DBBF2',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -55,7 +55,7 @@ export default {
     {
       id: 5,
       images: [
-        'https://lh3.googleusercontent.com/bKw2aR8loV-tk8i1v9InH1P3Zob1D4tXHB6sZlYSQEU7BavQFM67UsuHdqHnwDvr_sBUOp1m3sA8Z0btqUJOWy-uQcQ0mfr5H02Ygba61fo7Ttz1Kg8AyF5Sr_XSdHD5sV8g6T_EJiIvpkMZ400g_EDvVOe6AKgWX4ePJngqssbvdJMFMr0Os1IJzhFGGY4jdj_3Bj2_9ZAcflk6J_fwwN6aS2fZN5hLvKXaIuLJuUzLzNg34r3Fo6xY-xrASW0hXmOSmaG56qsRXKNqLf9pF1qXcUlb5wHM_HKKMPzkrgWb0DvaxBLLR6W80xno1bK6eeEReCmAmTwg1XXXox6fkyhewMgUNe9y4pHUzXWsqF2iteuDx-teyAEtCxGAO0HHvl-2Pcxwn64t33QCuO4ZpREO83d5gz9mFkDWl1WzIpn2y9czLuXFBcmC6HLSbLGKJuUKLhgSoFpgGWngF-SA0ylNppZUq7IUrjB4_PAP_8JIvwo9u0WEu0TAk8jiZhQOGX-ydb4lCD5Hkxd7JbyNKYLUyW7vTIqiruk35zS20EBSgQ0PcV-OEiUzPOP7MK4LwUUzo5wr-teewO3dYXAgCd9ApVFB76Jsp60ewYM6_wp_XPbnsO-JbKu2d3lvYXYn2y6rmeBVsBvU4xaKW_RBBF7vmVViJiyIxP0CMp8HduH6-ijoEhkaHbLYsH_wnw=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/145535053_3747022138711688_4737205472260775316_o.jpg?_nc_cat=102&ccb=2&_nc_sid=0debeb&_nc_ohc=79duJTUXTWAAX_ysvqz&_nc_ht=scontent.fsyq2-1.fna&oh=a6807a4318d0ccd95810358e63c317f5&oe=603EDAB4',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -67,7 +67,7 @@ export default {
     {
       id: 6,
       images: [
-        'https://lh3.googleusercontent.com/zhGAGQO2uzqYmz_69WmPyGqXDh5V0-IyBshtXjxgiDIkrRRRF86gG1MiWDvUGg1h-7fx3ROOUbb92kCYYyl1Su6_c-0l36V_YlYGwTgQfvzcDYr73QtbJvAW6KMiMdVhr9mOrSoH5xytIVgWh94xb3aV5Kkel8iyMHf8iNVlJdhvBhVNbUQGm-sXaKCuqCeLqKUKGt0XS9hrzCRv6UswjyVDigUohK74xxUlLm15mjiSGt1948hHNOFJjvmmySBOifVyDq1xxsbq6Rj5Rz5GVEc8mLDRxwcFyH8VPXoXkGPykXaporYcvU1M9yKIZlHk7qMAgR5LMCi42EBVHlqeYBd_WcM6a6FfrlxY942qVdCCoQ1lQHKqeHBPUjnROJAgZz-hSVtSh9EB5eWMMc-0KJngJByeQJrLJYPwwqtKO76bo1MVplz9LS-7RqA_YOH9-LbL0FkWFxfTvwMLCQt0-XyO7mj9eedTkCRecsqToOLWZTEVnxF3W7Rz5RDBxG4ZLR0WwJZ39Akc1Wqni8ztr8TYL7-BTw_FBRyvH9ax4Uryh2Hk-APWLPiQ6McbBEFAa69OHnO7ryuShBt1tSgaYgLNR53ZbvTrHSvON1nuIrnuAl02wID-u6w_cOQzdUnflcAIpXkh867i8fnQFOWgYnCMeutyPIGNMBRdR_bh1P0a_4VZvh3l82Urw-J7eA=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/143846278_3747020788711823_6782760630763754692_o.jpg?_nc_cat=100&ccb=2&_nc_sid=0debeb&_nc_ohc=orjGfMb84CwAX-KUZgF&_nc_ht=scontent.fsyq2-1.fna&oh=bed558650eb1f4f02e23c59e622e293e&oe=603D3D06',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -79,7 +79,7 @@ export default {
     {
       id: 7,
       images: [
-        'https://lh3.googleusercontent.com/tcKSGNF3uWc2Gpyc5EKdYmHq6Xfxg_ceQl5NcqMgofRXRZkmFkd9X17XWwoK0TdTVyhtgqlzhgEmBur1lazr2lJVY3ETVFy2UlO5zadnTn4S4PD6dIFATpKpmAezN5eY7G0mlTSG5uqnsNNGxCG3q4Szcn2q3aB57a4wsxz3coz0ANqERuUoX1KSUkn0dWUcQPvn3StQSqD4HD-ueBkOy1S_F0vp8opg-IWZ8kuARBz4i0_Qy9tNSdX1id57k_-GsurmqEawxPTuCF8_X64TpFWChRV7U-uCx9RSer87BLvWvjjaLrzR5SGEUIuPxSBUEIpuRlqUJAn5K5ywGqyjVZUTLaFks1ZJk5h4oEPJF8duXvpnZE-uf8ift4rP-XDJvz9w8qcSkFI4lkH3YAzQ_qge_apxw8p7_QoijN0U9HZaIVZ1EowszI2k-o8VZoFrOy4uXvtpOlRs60OHv2K3oYx2d6h01bD-D6_RDWaEEprpFRTrEzCiJHqRVV9fkJ24RWl2Z6dxLkDwK0_EupsGnENmQJfAKLw468iXn4Cg5v-EJJCqk78NQt9BzzV3ryxCN-xgjmfEXVVxRYovMGCytBKjowGKGPFikKakx6tRn_UxsatI96ReaWrG7N303MkxHfwGPuSHTCxQy3UZaO4u7V-QN-zL6dlPKLaxPx0B-TSVpa_N_uTWEcPShCQNQg=w660-h880-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/144970277_3747021552045080_6349946052614428093_o.jpg?_nc_cat=110&ccb=2&_nc_sid=0debeb&_nc_ohc=ZuyIFa2zelQAX_0ohZF&_nc_ht=scontent.fsyq2-1.fna&oh=4e6b5541eb074c7f6a9e73ebd53556b6&oe=603DD6E9',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -92,7 +92,7 @@ export default {
     {
       id: 8,
       images: [
-        'https://lh3.googleusercontent.com/I3tUkknk1FnX4Mb6QuPmuvYMxoUeZCBpy57W3DtpgNth3RPh747KXI1EM48ponHUkemJt5zlkH47t8w7vp3_-tMb1tgxRm0ycVWLiqQl_-DwSTUcpuyrvT89PiL2tzis3EHxqrGWKVsX6Lx6BSxMD2xRLupKTRP8M1u5vuE-dLU_uHob4kfMvOxyi20EsXUnmWA6VwsZmCLvS_JDIVRZ4p_CnUAY9zLnqZL6khvv4C4Jb9ywYTtR3fuEbYY49lkF3S6O1WEjWhrP-sOoTkxRZfOhcSqym6BrF6M9fl1IXCJwNV_ytGSbXOYV3cvOEn0dqo3S0wURNn9WrQdh77mrfxEKvDzsn2erJ_V0d_Udf0eytiaMTIEFoZY7cM6t9WD11eDLBZV8H_1q1lnlXHfzYmkzban9GI1aR8YljNVvFWLhXyqT4zpU1wbTs5wFa_I_ikLF0HwNPrSmRbFYiV1Vql8m6PYPafcxeWm51y5QWqhYeyO_oVAzzFkmRWevCyLF49hPw6utjxg7pnSWAe9voIe4KANcBjIplB3WZ1m3Pyttt156n8p9ZP8gdq4i-mbWaKhSUzt9N4K2X2_aassHdFk-frluifvZqaIIgTOs8xIlM6L96nsCjALgyd3enFBq2OHNj6GjNPz8Kww88ZJKVcL4vKOUHUJ3f8mE7_aYipcv55M0OexCb-UT1hHxWQ=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/144678030_3747021392045096_8436778479516796370_o.jpg?_nc_cat=103&ccb=2&_nc_sid=0debeb&_nc_ohc=pK_8lfaFE8EAX9E4D6X&_nc_ht=scontent.fsyq2-1.fna&oh=3f4404894980d20a16ccaf9197915c4c&oe=603EC81C',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -104,7 +104,7 @@ export default {
     {
       id: 9,
       images: [
-        'https://lh3.googleusercontent.com/zhfpj8K0fh8MClDZagmTqmWOA7s9IYimqtJ4uhjGUB_CNgTgqIIJIoQARIYB9VVMta5K_n7nCoypYPw6QY7IBPTTkSQ4Ipq8LaajIp2xFikjJQ3XqE_uHcco3x_0J0rcQKN7YT3qLJIDtjxDXPWG2aD_Whx64wBBkeu9GGEdGCbx5nOKAoNGEUgWINXKJLDy4kZpLXwRRuNKIxhrg0cPYn8sE9OF06WWXoEqdzhOSl643uyxldvqTVEhdJ6aSH1IHRbLQmig8iGJlyCNbivwkZt72eVC4K0fpREnlEVOsiG3KYPYW6xNN87So_wV8XLGDs5vIRvjf4DbcwZ2qVrP2hNvcbT4lpR5uGthu6d_QnQVxYc3zlxWR5oBAo5svzi-JLSNaIEf-t68plsH9Wxh4q-8C7fUDk6cP9AS-zkdYG4Wh-RbEqpEH7hbeS_38suT8ilAtimu9yLj2Eqay1t7PS01d5Og2hofq5fY8Ii66LUb4eMbDDAs_2OyGLd19jF64RhX1ILcqpnFimRcVIl8o1SopMGTY76vbShpjk5MMH0l3dPedZd68SYRr8VHQlHNoi04Js9vGXbHMuj_grxK61dmOzeOHauOowTZK1Y_CSJhgYdXKmfL6vgjpsLV5DVmQ8u_9G-bbT083KS08sxreo5bYR3gxLDaBsH1a6AHn2GPuMkdFb2epqnnZotulg=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/144452779_3747021335378435_7879799849178805391_o.jpg?_nc_cat=108&ccb=2&_nc_sid=0debeb&_nc_ohc=EssftLoPZfkAX81EYuk&_nc_ht=scontent.fsyq2-1.fna&oh=1b3d6f867d8917235da9226dd9ec38fe&oe=603C33B6',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
@@ -116,7 +116,7 @@ export default {
     {
       id: 10,
       images: [
-        'https://lh3.googleusercontent.com/MgJJlB91CoAizahW_aUhzpFTdw8X0Qi6E1GsGOX3_cIloYlbTfKbix8DB9mj15pljMgFM3Oe8gZuJkZ4fypNCy62esJRm-3ru8tdxjJKYyIwZCa48jNKmEasU1CC82GlylN_qYZiLFhppPqmsuwv-moJV65fncAmfe0QezXOVW8qwi4M5w8cZh-JIEZgcOocnu7GetLVDNRwxLoEHJfJnZlp2sCOzTAJ56lvwDi6Lp4p-WhYAvrCky0Ik6GuHWLM1tNsR3YZrhnfFx_aGN34xMm5VC8qHR2noGteEKGtolszkRWR3JrWiWv7YJygGvuPrwQpONW5K594J99lbnxeMXZhsGq4NVdTndSu9h7dLqVvtBJeTttKtXn4lTWZrgiZ8CniWt7cxLcWPZWcRl7yo3_h6F8bKP4Sj00a0mrW14N5yQLtlIk3RxFfZaabnP4lH6l2vmAK-QlRPquM1KAs9-dnnyEsMNnZYQI_d6ngdlWCF6OR5tBJ_j0tubKPlIYaK6PcsjaYeW9mWvh2qMjHOjpL9Dgemxg1Ux2kXQ5O6CuzCDh6t84_TOAeOFDC9m1kYXrjn8U_0lRnFgMFRY0J6HF73wBNWxFMhvWWQ6MDcNAc4bxHLLdy9CD6Lx-EK81OIKG8JjkCYDts04MIeoV74eR38JxZnlsDJHhxerTsQHutI5QtgJ1ahlM1Z0F6zQ=w703-h937-no?authuser=0.jpg',
+        'https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.0-9/144767677_3747020965378472_7268777341578622204_o.jpg?_nc_cat=110&ccb=2&_nc_sid=0debeb&_nc_ohc=LNyE94C5d7UAX_Wu2Dj&_nc_ht=scontent.fsyq2-1.fna&oh=3cf7868b32b0aa1cd2f68a4114bb51e4&oe=603E5A3A',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
         'https://arepa.s3.amazonaws.com/camiseta.png',
---
+++ tienda-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6a29113-3591-41d7-a5dd-a6289107ce57'
+};
